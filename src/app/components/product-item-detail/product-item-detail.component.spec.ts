76423b3bb786e6ff217d5d74e377ca1504189b49
ntn1999 <nam14nt@gamil.com>
2023-04-18 22:22:16 +0700

    first commit

---
+++ src/app/components/product-item-detail/product-item-detail.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ProductItemDetailComponent } from './product-item-detail.component';
+
+describe('ProductItemDetailComponent', () => {
+  let component: ProductItemDetailComponent;
+  let fixture: ComponentFixture<ProductItemDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProductItemDetailComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductItemDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
